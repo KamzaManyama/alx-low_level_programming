03ade952bf87bcebb4bccb2715d28193da6e77f4
KamzaManyama <kmanyama009@gmail.com>
2023-07-19 13:18:46 -0700

    betty style cnfig

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,15 +1,14 @@
 #include "main.h"
 
 /**
- * orint_last_digit - Prints the last digit of a number
- * @c: The character to be checked
+ * print_last_digit - Prints the last digit of a number
+ * @n: The character to be checked
  *Return: Value of the last digit of number
  */
- 
 int print_last_digit(int n)
 {
-	int last;
-	
+int last;
+
 	last = n % 10;
 	if (last < 0)
 	{
