00cea78df9cf692adc78b4a7b817474808e73c58
KamzaManyama <kmanyama009@gmail.com>
2023-07-20 13:16:31 -0700

    Collaboration is multiplication

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - checks for lowercase letters
+ * @a: the chaaracyer to be checked
+ * @b: the second integer
+ * Return: 0
+ */
+int mul(int a, int b)
+{
+	int result = a + b;
+
+	return (result);
+}
