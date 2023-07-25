603a878c929e8505a7d614149102fb38b9ad2dac
KamzaManyama <kmanyama009@gmail.com>
2023-07-25 16:02:23 -0700

    I do not fear computers. I fear the lack of them

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * _puts - print a string
@@ -9,7 +10,7 @@ void _puts(char *str)
 {
 	for (; *str != '\0'; str++)
 	{
-		_putchar(*str);
+		putchar(*str);
 	}
-	_putchar('\n');
+	printf("\n");
 }
