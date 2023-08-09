f87a035be7c362c52e44b499cd90877b8524b0f1
KamzaManyama <kmanyama009@gmail.com>
2023-08-09 14:43:13 -0700

    what you answer to

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,14 +1,18 @@
-#include <stdio.h>
-#include "main.h"
+#include<stdio.h>
+#define UNUSED(x) (void)(x)
 
 /**
- * main - prints the name of program
- * @argc: jkd
- * @argv: fdlvd
- * Return: Always
- */
-int main(argc __attribute__((unused)),char *argv[])
+* main - function
+*
+* @argv: the chaine of chaine
+* @argc: the int
+* Return: Always 0.
+*/
+
+int main(int argc, char **argv)
 {
-	printf('%s\n\',*argv);
+	UNUSED(argc);
+	printf("%s\n", argv[0]);
+
 	return (0);
 }
